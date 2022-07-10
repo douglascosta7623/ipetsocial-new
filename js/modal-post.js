f83086df96e0934cb1996c5f6a26c1000e734e3d
Jose Bezerra <jbezerra@magnasistemas.com.br>
2022-07-10 10:56:32 -0300

    Refatoração do modal nas páginas

---
+++ js/modal-post.js
@@ -0,0 +1,40 @@
+const modalPost = document.querySelector(".modal-post");
+const openModal = document.querySelector("#open__modal");
+const isVisible = document.querySelector("#is-visible");
+const closed = document.querySelector("#close");
+
+openModal.addEventListener("click", (e) => {
+	toggleModal(e);
+	loadModal();
+});
+
+modalPost.addEventListener("click", (e) => {
+	toggleModal(e);
+});
+
+if (closed) {
+	closed.addEventListener("click", (e) => {
+		toggleModal(e);
+	});
+}
+
+function counter(event) {
+	const add = document.querySelector("#add");
+	add.innerHTML = event.target.value.length
+}
+
+function toggleModal(e) {
+	if (e.target.id || e.key == "Escape") {
+		// isVisible.classList.add("is-visible");
+    modalPost.innerHTML = '';
+	} else {
+		// isVisible.classList.remove("is-visible");
+	}
+}
+
+async function loadModal() {
+	const request = await fetch("../pages/modal/modal-post.html");
+	const modal = await request.text();
+	console.log(modal)
+	modalPost.innerHTML = modal;
+}
